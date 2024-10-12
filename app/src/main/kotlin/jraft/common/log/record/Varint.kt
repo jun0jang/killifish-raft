f86e1339257626a0b41e1f5290dff88652322574
jun0jang <wnsdud3256@gmail.com>
2024-10-12 16:27:37 +0900

    feat: add writeUnsignedVarint

---
+++ app/src/main/kotlin/jraft/common/log/record/Varint.kt
@@ -0,0 +1,65 @@
+package jraft.common.log.record
+
+import java.nio.ByteBuffer
+
+object Varint {
+
+    fun writeVarint(value: Int, buffer: ByteBuffer) {
+        writeUnsignedVarint(zigZagEncode(value), buffer)
+    }
+
+    fun writeUnsignedVarint(value: Int, buffer: ByteBuffer) {
+        var actualValue = value
+
+        /**
+         * 0x7F = 127 = 0000 0000 0000 0000 0000 0000 0111 1111
+         * 0x7F.inv() = 1111 1111 1111 1111 1111 1111 1000 0000
+         * (value and 0x7F.inv()) != 0
+         *  - value가 7 비트 이내로 표현 되지 않음을 의미
+         */
+        while (actualValue and 0x7F.inv() != 0) {
+            /**
+             * 0x7F = 127 = 0111 1111
+             * 0x80 = 128 = 1000 0000
+             * actualValue & 0x7F or 0x80
+             * - 7비트를 가져옴
+             * - 8번째 비트를 1로 세팅 (Continuation bit)
+             */
+            buffer.put(((actualValue and 0x7F) or 0x80).toByte())
+            // 사용한 7비트를 제거
+            actualValue = actualValue ushr 7
+        }
+
+        buffer.put(actualValue.toByte())
+    }
+
+    fun sizeOfVarint(value: Int): Int {
+        return sizeOfUnsignedVarint(zigZagEncode(value))
+    }
+
+    fun sizeOfUnsignedVarint(value: Int): Int {
+        // Protocol buffers varint encoding is variable length, with a minimum of 1 byte
+        // (for zero). The values themselves are not important. What's important here is
+        // any leading zero bits are dropped from output. We can use this leading zero
+        // count w/ fast intrinsic to calc the output length directly.
+        //
+        // 0일 경우
+        //   (38 - leadingZeros) / 7 = 0
+        //   leadingZeros / 32 = 1
+        // 1일 경우
+        //   (38 - leadingZeros) / 7 = 1
+        //   leadingZeros / 32 = 0
+        // 11111111_11111111_11111111_11111111일 경우
+        //   (38 - 0) / 7 = 5
+        //   0 / 32 = 0
+        // leadingZeros 0, 1, 2, 3 까지는 return 5
+
+        val leadingZeros = Integer.numberOfLeadingZeros(value)
+        return (38 - leadingZeros) / 7 + leadingZeros / 32
+    }
+
+    // (n << 1) ^ (n >> 31) is ZigZag encoding
+    fun zigZagEncode(value: Int): Int {
+        return value.shl(1).xor(value.shr(31))
+    }
+}
---
+++ app/src/test/kotlin/jraft/common/log/record/VarintTest.kt
@@ -0,0 +1,75 @@
+package jraft.common.log.record
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import java.nio.ByteBuffer
+
+class VarintTest : FreeSpec({
+
+    "zigZagEncode" {
+        // Given
+        val pairs = listOf(
+            -1 to 1,
+            1 to 2,
+            -2 to 3,
+            2 to 4,
+            -64 to 127,
+            64 to 128,
+        )
+
+        for ((value, expected) in pairs) {
+            // When
+            val encoded = Varint.zigZagEncode(value)
+
+            // Then
+            encoded shouldBe expected
+        }
+    }
+
+    "writeVarint" {
+        // Given
+        val pairs = listOf(
+            1 to byteArrayOf(1, 0, 0, 0, 0),
+            // 128 == 0b1000_0000
+            128 to byteArrayOf(-128, 1, 0, 0, 0),
+            // 256 == 0b1_0000_0000
+            256 to byteArrayOf(-128, 2, 0, 0, 0),
+            // 2147483647 == 0b0111_1111_1111_1111_1111_1111_1111_1111
+            2147483647 to byteArrayOf(-1, -1, -1, -1, 7),
+        )
+        val buffer = ByteBuffer.allocate(5)
+
+        for ((value, expected) in pairs) {
+            // When
+            Varint.writeUnsignedVarint(value, buffer)
+
+            // Then
+            assertArrayEquals(
+                expected,
+                buffer.array(),
+                "Implementations do not match for integer=$value",
+            )
+            buffer.clear()
+        }
+    }
+
+    "testSizeOfVariant" {
+        // Given
+        val pairs = listOf(
+            0b11111111_11111111_11111111_11111111 to 1, // -1's size is 1
+            0b00000000_00000000_00000000_00000011 to 1, // 3's size is 1 bytes
+            0b00000000_00000000_00000000_10000000 to 2, // 128's size is 2 bytes
+            0b10000000_00000000_00000000_00000000 to 5, // -2147483648's size is 5 bytes
+            0b01000000_00000000_00000000_00000000 to 5, // 1073741824's size is 5 bytes
+            0b00010000_00000000_00000000_00000000 to 5, // 268435456's size is 5 bytes
+            0b00000100_00000000_00000000_00000000 to 4, // 268435456's size is 5 bytes
+        ).map { (value, expected) -> value.toInt() to expected }
+
+        for ((value, expected) in pairs) {
+            val size = Varint.sizeOfVarint(value)
+
+            size shouldBe expected
+        }
+    }
+})
